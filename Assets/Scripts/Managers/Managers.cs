428a7d771cd4916acd2dcd63b9f00dc85b82424b
MinseokChoi12 <minseok1268@gmail.com>
2022-08-03 21:23:42 +0900

    First Commit

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance; // static을 사용하여 유일성을 보장한다.
+    public static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고 온다.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+        
+    }
+}

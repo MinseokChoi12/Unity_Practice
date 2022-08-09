af02f7a482a10f320f41e6d2cc175a7cd4da1953
MinseokChoi12 <minseok1268@gmail.com>
2022-08-09 14:42:29 +0900

    MyVector3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 1. À§Ä¡ º¤ÅÍ (ÁÂÇ¥·Î ÀÎ½Ä)
+// 2. ¹æÇâ º¤Ã³
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float b)
+    {
+        return new MyVector(a.x * b, a.y * b, a.z * b);
+    }
+
+}
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -9,7 +39,13 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        MyVector a = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector b = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = b - a;
+
+        dir = dir.normalized;
+
+        MyVector newPos = a + dir * _speed;
     }
 
     void Update()

5ac3ba1ab6f5b77133ce5f31655fdf0f051cef94
MinseokChoi12 <minseok1268@gmail.com>
2022-08-09 15:22:06 +0900

    Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,61 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 1. 위치 벡터 (좌표로 인식)
-// 2. 방향 벡처
-struct MyVector
-{
-    public float x;
-    public float y;
-    public float z;
-
-    public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
-    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
-
-    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
-
-    public static MyVector operator +(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-
-    public static MyVector operator -(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-
-    public static MyVector operator *(MyVector a, float b)
-    {
-        return new MyVector(a.x * b, a.y * b, a.z * b);
-    }
-
-}
-
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
-
     void Start()
     {
-        MyVector a = new MyVector(10.0f, 0.0f, 0.0f);
-        MyVector b = new MyVector(5.0f, 0.0f, 0.0f);
-        MyVector dir = b - a;
-
-        dir = dir.normalized;
-
-        MyVector newPos = a + dir * _speed;
+        
     }
-
+    float _yAngle = 0.0f;
     void Update()
     {
+        _yAngle += Time.deltaTime * _speed * 100;
+
+        // 절대 회전값
+        //transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
+
+        // +,- delta
+        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * _speed * 100, 0.0f));
+
+        // 원하는 방향으로 회전
+        // Quaternion: Vector3로 사용하면 gimbal lock 현상이 발생하기 때문에 이를 방지하기 위해 사용
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+<<<<<<< HEAD
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.05f);
+
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+            
         if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.back);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.05f);
+
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+
+        }
+
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.left);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.05f);
+
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+
+        }
+
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        {
+            //transform.rotation = Quaternion.LookRotation(Vector3.right);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.05f);
+
+            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+
+        }
+
+=======
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        if (Input.GetKey(KeyCode.S))
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        if (Input.GetKey(KeyCode.A))
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+>>>>>>> parent of 38f5f6c (Set Player)
     }
 }
